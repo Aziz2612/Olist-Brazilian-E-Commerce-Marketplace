4ded3f6aa03ddc8862d04178ce493ba5cefdffbf
Abdalaziz Ahmed <132609624+Aziz2612@users.noreply.github.com>
2026-07-15 14:26:11 +0300

    Update 01_Sales_Analysis.sql

--- SQL Queries/01_Sales_Analysis.sql
+++ SQL Queries/01_Sales_Analysis.sql
@@ -1,35 +1,45 @@
 -- Monthly Performance -- 
 
-WITH payments AS(
-SELECT op.order_id , SUM(op.payment_value) payment_value
-FROM olist_order_payments_dataset op
-JOIN olist_orders_dataset o ON o.order_id = op.order_id
-WHERE o.order_status NOT IN ('canceled', 'unavailable')
-GROUP BY 1
+WITH payments AS (
+    SELECT op.order_id, SUM(op.payment_value) AS payment_value
+    FROM olist_order_payments_dataset op
+    JOIN olist_orders_dataset o ON o.order_id = op.order_id
+    WHERE o.order_status NOT IN ('canceled', 'unavailable')
+    GROUP BY 1
+),
+order_sellers AS (
+    SELECT oi.order_id, COUNT(DISTINCT oi.seller_id) AS order_unique_sellers
+    FROM olist_order_items_dataset oi
+	JOIN olist_orders_dataset o ON o.order_id = oi.order_id
+	WHERE o.order_status NOT IN ('canceled', 'unavailable')
+    GROUP BY 1
 ),
 ranked_reviews AS (
-SELECT order_id , review_score, row_number () OVER ( PARTITION BY order_id ORDER BY review_answer_timestamp DESC) AS rn
-FROM olist_order_reviews_dataset 
+    SELECT rr.order_id, rr.review_score, ROW_NUMBER() OVER (PARTITION BY rr.order_id ORDER BY rr.review_answer_timestamp DESC) AS rn
+    FROM olist_order_reviews_dataset rr
+	JOIN olist_orders_dataset o ON rr.order_id = o.order_id
+	WHERE o.order_status NOT IN ('canceled', 'unavailable')
+
 ),
 latest_reviews AS (
-SELECT order_id, review_score
-FROM ranked_reviews
-WHERE rn = 1
+    SELECT order_id, review_score
+    FROM ranked_reviews
+    WHERE rn = 1
 )
-SELECT DATE(o.order_purchase_timestamp,'start of month') as date, 
-COUNT(DISTINCT o.order_id) orders, 
-SUM(p.payment_value) GMV ,
-COUNT(DISTINCT oc.customer_unique_id) customers,
-COUNT(DISTINCT oi.seller_id) AS unique_sellers,
-ROUND(SUM(p.payment_value) / COUNT(DISTINCT o.order_id),2) AOV, 
-ROUND(AVG(r.review_score),2) average_scores, 
-CAST(AVG(CASE WHEN o.order_delivered_customer_date IS NOT NULL THEN julianday(o.order_delivered_customer_date) - julianday(o.order_purchase_timestamp) ELSE NULL END) AS INT) AS avg_delivery_days
+SELECT 
+    DATE(o.order_purchase_timestamp, 'start of month') AS date, 
+    COUNT(DISTINCT o.order_id) AS orders, 
+    SUM(p.payment_value) AS GMV, 
+    COUNT(DISTINCT oc.customer_unique_id) AS customers,
+    SUM(os.order_unique_sellers) AS unique_sellers, 
+    ROUND(SUM(p.payment_value) / COUNT(DISTINCT o.order_id), 2) AS AOV, 
+    ROUND(AVG(r.review_score), 2) AS average_scores, 
+    CAST(AVG(CASE WHEN o.order_delivered_customer_date IS NOT NULL THEN JULIANDAY(o.order_delivered_customer_date) - JULIANDAY(o.order_purchase_timestamp) ELSE NULL END) AS INT) AS avg_delivery_days
 
 FROM olist_orders_dataset o 
 JOIN payments p ON o.order_id = p.order_id 
 JOIN olist_customers_dataset oc ON o.customer_id = oc.customer_id
-LEFT JOIN olist_order_items_dataset oi ON o.order_id = oi.order_id
+LEFT JOIN order_sellers os ON o.order_id = os.order_id 
 LEFT JOIN latest_reviews r ON o.order_id = r.order_id
-WHERE o.order_status NOT IN ('canceled' , 'unavailable')
+WHERE o.order_status NOT IN ('canceled', 'unavailable')
 GROUP BY 1;
-
